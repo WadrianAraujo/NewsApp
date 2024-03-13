4920c7632fa346894f7d2c56eceb234cadf85c67
Wadrian Araujo De Moura <wadrian.araujo@gmail.com>
2024-03-13 15:43:18 -0400

    Add: initial setup and dependencies

--- app/src/main/java/com/wax/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/wax/newsapp/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.wax.newsapp
+package com.wax.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wax.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
